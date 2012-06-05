865ad40d379a75a3f1149856894da421d62c31a8
Hans-Peter Wolf <hapewolf@gmail.com>
2012-06-05 23:24:39 +0200

    + Initial source file

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ * Created: 05.06.2012 20:47:04
+ *  Author: Hans-Peter
+ */ 
+
+
+#include <avr/io.h>
+#include "CM_510.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+	return 0;
+}
