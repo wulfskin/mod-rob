17560fdc56dca00046b63e8e9aa5bda1b592fe81
walteruzzu <s104169@student.dtu.dt>
2012-06-06 13:46:37 +0200

    -removed our headers from /include folder

--- include/error.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERROR_H_
-#define ERROR_H_
-
-void error(void);
-void error_blocking(void);
-
-
-#endif /*ERROR_H_ */
--- include/macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MACRO_H
-#define __MACRO_H
-
-#define SET(REGISTER,BIT) REGISTER|=(1<<BIT)
-#define CLEAR(REGISTER,BIT) REGISTER &= ~(1<<BIT)
-#define TOGGLE(REGISTER,BIT) REGISTER^=(1<<BIT)
-
-#endif /* __MACRO_H */
