2a2ca423f8f7218788387c46cdf3ddd9d3344f9b
Hans-Peter Wolf <hapewolf@gmail.com>
2012-06-06 13:36:25 +0200

    + Add macro GET to read a bit * Change existing macros to be more safe

--- src/macro.h
+++ src/macro.h
@@ -1,8 +1,9 @@
 #ifndef __MACRO_H
 #define __MACRO_H
 
-#define SET(REGISTER,BIT) REGISTER|=(1<<BIT)
-#define CLEAR(REGISTER,BIT) REGISTER &= ~(1<<BIT)
-#define TOGGLE(REGISTER,BIT) REGISTER^=(1<<BIT)
+#define GET(REGISTER,BIT) ((REGISTER) & (1<<(BIT)))
+#define SET(REGISTER,BIT) ((REGISTER) |= (1<<(BIT)))
+#define CLEAR(REGISTER,BIT) (REGISTER &= ~(1<<(BIT)))
+#define TOGGLE(REGISTER,BIT) ((REGISTER) ^= (1<<(BIT)))
 
 #endif /* __MACRO_H */
