dfc1ca6401dbf0ac9730cb62466c00b8e1c30b0e
cautoo <cautoo@gmail.com>
2012-06-07 11:10:13 +0200

    Newest Zigbee - working...

--- src/serialzigbee.c
+++ src/serialzigbee.c
@@ -1,10 +1,10 @@
-#include "serialzigbee.h"
 #include <avr/io.h>
+#include "serialzigbee.h"
 
 void serial_set_zigbee() {
 	
-  //Output on the 3 pins
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
+  //Output on the 3 pins -MAY NOT BE NESSESARY
+  //DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
 	
   PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
   PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
@@ -12,13 +12,11 @@ void serial_set_zigbee() {
 }
 
 void serial_set_wire() {
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
-  
-  //Dunno yet, with next line
-  //PORTD &= ~0x80; //PORT_LINK_PLUGIN = 0;   // no pull up
+  //-MAY NOT BE NESSESARY
+  //DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
   
   PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-  PORTD |= ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
+  PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
   
 }
 
