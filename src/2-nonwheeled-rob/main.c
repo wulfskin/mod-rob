017763264e81f6b913f484f182d80e2cd54ab68d
walteruzzu <s104169@student.dtu.dt>
2012-06-14 17:14:54 +0200

    going straight more less

--- src/2-nonwheeled-rob/main.c
+++ src/2-nonwheeled-rob/main.c
@@ -12,9 +12,15 @@
 #include "../serialzigbee.h"
 #include "../io.h"
 
-#define NUM_ACTUATOR 3
+#define CENTER 512
+#define TWIST_AMPLITUDE 20
+#define TWIST_OFFSET 0
 
-int16_t turn=0;
+#define LOOP 100000
+
+#define STEP 250
+
+int16_t turn=0,diff=0;
 
 void something(void){
 	LED_TOGGLE(LED_MANAGE);
@@ -23,17 +29,33 @@ void something(void){
 	//printf("%c\n", data);
 	
 	if(data=='a'){
-		turn = turn>=120 ? 120 : turn+20;
+		turn = turn>=180 ? 180 : turn+20;
 		printf("turn=%d\n", turn);
 	}		
 	else if(data=='d'){
-			turn = turn<= -120 ? -120 : turn-20;
+			turn = turn<= -180 ? -180 : turn-20;
 			printf("turn=%d\n", turn);
 	}
 	else if(data=='w'){
 		turn=0;
 		printf("turn=%d\n", turn);
 	}					
+}
+
+void wait_for_position(int number, int setoffset) {
+			//positions[0]=512+30;
+			dxl_write_word(number,GOAL_POSITION_L,setoffset);
+			uint32_t count=0;
+			do
+			{
+				diff=(setoffset)-dxl_read_word(number,PRESENT_POSITION_L);
+				if (diff < 0)
+				diff = -diff;
+				_delay_us(1000);
+				count+=1;
+				if ((count&0x007F) == 0x0000)
+				 printf("Motor %d now has counted to %u\n", number, count);
+			} while ( diff>5 && count<LOOP );	
 }
 
 		//uint8_t ids[3]={4,7,8};
@@ -51,27 +73,54 @@ void something(void){
 		sei();
 		io_init();
 		int i=0,j=0,readd=0,moving=0;
-		printf("test\n");
+		printf("start\n");
 		dxl_write_word(4,GOAL_POSITION_L,512);
 		dxl_write_word(7,GOAL_POSITION_L,512);
-		dxl_write_word(8,GOAL_POSITION_L,512);
+		//dxl_write_word(8,GOAL_POSITION_L,512);
+		
+		
 		while(!(BTN_DOWN_PRESSED));
-		for(i=0;i<500;i++){
-			
+		//for(i=0;i<500;i++){
+		while(1) {	
 			//positions[0]=512+30;
 			//positions[1]=512+70;
 			//positions[2]=512+turn-70;
 			//motor_sync_move(3,ids,positions);
+			/*
+						//positions[0]=512+30;
+						dxl_write_word(4,GOAL_POSITION_L,CENTER+TWIST_OFFSET+TWIST_AMPLITUDE);
+						uint32_t count=0;
+						do
+						{
+							diff=(CENTER+TWIST_OFFSET+TWIST_AMPLITUDE)-dxl_read_word(4,PRESENT_POSITION_L);
+							if (diff < 0)
+							diff = -diff;
+							_delay_us(10);
+							count+=1;
+						} while ( diff>5 && count<LOOP );
+
+			*/
+			wait_for_position(4,CENTER+TWIST_OFFSET+TWIST_AMPLITUDE);
+			printf("end loop 1,\n");
+			//} while (dxl_read_byte(4, MOVING));
 			
-			//positions[0]=512+30;
+			/*dxl_write_word(7,GOAL_POSITION_L,CENTER+turn+STEP);
+			count=0;
+			do
+			{
+				diff=(CENTER+turn+STEP)-dxl_read_word(7,PRESENT_POSITION_L);
+				if (diff < 0)
+				diff = -diff;
+				_delay_us(10);
+				count+=1;
+			} while ( diff>5 && count<LOOP );*/
+			wait_for_position(7, CENTER+turn+STEP);	
+			printf("end loop 2,\n");
 			
-			dxl_write_word(4,GOAL_POSITION_L,527+30);
-			_delay_ms(60);
-			positions[0]=512+90;
-			positions[1]=512+turn-90;
-			motor_sync_move(2,ids,positions);
+			//positions[0]=CENTER-turn+STEP;
+			//positions[1]=CENTER;//+turn-STEP;
+			//motor_sync_move(2,ids,positions);
 			
-				
 			//_delay_ms(1);
 /*			do{
 				moving=0;
@@ -86,19 +135,42 @@ void something(void){
 					moving|=readd;
 				}
 			}while(moving);*/								
-			_delay_ms(200);
+			//_delay_ms(200);
+		
 			//positions[0]=512-30;
 			//positions[1]=512-70;
 			//positions[2]=512+turn+70;
 			//motor_sync_move(3,ids,positions);
 			
-			dxl_write_word(4,GOAL_POSITION_L,527-30);
-			_delay_ms(60);
-			positions[0]=512-90;
-			positions[1]=512+turn+90;
-			motor_sync_move(2,ids,positions);
-			
-			_delay_ms(200);
+			/*dxl_write_word(4,GOAL_POSITION_L,CENTER+TWIST_OFFSET-TWIST_AMPLITUDE);
+			count=0;
+			do{
+				diff=(CENTER+TWIST_OFFSET-TWIST_AMPLITUDE)-dxl_read_word(4,PRESENT_POSITION_L);
+				if (diff < 0)
+				diff = -diff;
+				_delay_us(10);
+				count+=1;
+			} while ( diff>5 && count<LOOP );*/
+			wait_for_position(4, CENTER+TWIST_OFFSET-TWIST_AMPLITUDE);
+			printf("end loop 3,\n");
+			//} while (dxl_read_byte(4, MOVING));
+					
+			//_delay_ms(60);
+			//positions[0]=CENTER-turn-STEP;
+			//positions[1]=CENTER;//+turn+STEP;
+			//motor_sync_move(2,ids,positions);
+			/*dxl_write_word(7,GOAL_POSITION_L,CENTER+turn-STEP);
+			//_delay_ms(200);
+			count=0;
+			do{
+				diff=(CENTER+turn-STEP)-dxl_read_word(7,PRESENT_POSITION_L);
+				if (diff < 0)
+				diff = -diff;
+				_delay_us(10);
+				count+=1;
+			} while ( diff>5 && count<LOOP );*/
+			wait_for_position(7, CENTER+turn-STEP);
+			printf("end loop 4,\n");
 /*			do{
 				moving=0;
 				for(j=0;j<3;j++){
@@ -108,12 +180,15 @@ void something(void){
 					moving|=readd;
 				}
 			}while(moving); */
+
+			PrintErrorCode();
+			printf("Cycle,\n");
 			
-		}//closes for loop
+		//}//closes for loop
 			
 		
-		while(1) {
+		
 						
-		}
+		}// close infinite loop
 		return 0;
 }
